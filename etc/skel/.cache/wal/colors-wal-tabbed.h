9956e26497b79c56d134e7992beeadabf124a5fc
arcolinuxz <arcolinuxinfo@gmail.com>
2024-04-08 22:11:57 +0200

    wal folder

---
+++ etc/skel/.cache/wal/colors-wal-tabbed.h
@@ -0,0 +1,6 @@
+static const char* selbgcolor   = "#12161e";
+static const char* selfgcolor   = "#e1e4e9";
+static const char* normbgcolor  = "#7192B6";
+static const char* normfgcolor  = "#e1e4e9";
+static const char* urgbgcolor   = "#5E81AB";
+static const char* urgfgcolor   = "#e1e4e9";
