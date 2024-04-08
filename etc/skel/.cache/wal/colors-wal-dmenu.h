8a3bd24d7c262ba9b19c71c60f9ff2eedfd3e2ef
arcolinuxz <arcolinuxinfo@gmail.com>
2024-04-08 23:04:01 +0200

    update

--- etc/skel/.cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#e1e4e9", "#12161e" },
-	[SchemeSel] = { "#e1e4e9", "#5E81AB" },
-	[SchemeOut] = { "#e1e4e9", "#B2BAC6" },
-};
